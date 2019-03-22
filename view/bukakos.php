90effec9e88bf23c4e04c69dda7b5c138f811058
agashizinedine <agashimaster84@gmail.com>
2019-03-22 11:17:07 +0700

    .

--- view/bukakos.php
+++ view/bukakos.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Kosan Jaman Kini</title>
+	<title>Kosan Jaman Kini</title><!-- adsjfhas -->
 	<meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
